58b9053865025b8e3e7317bbb5d259f560772661
sreenithegate <sreenithegate@gmail.com>
2026-08-21 21:48:12 +0530

    Global localStorage safety override for undefined values

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+// SAFETY OVERRIDE: Prevent localStorage from returning "undefined" string
+const originalGetItem = Storage.prototype.getItem;
+Storage.prototype.getItem = function(key) {
+  const value = originalGetItem.call(this, key);
+  if (value === 'undefined' || value === 'null') {
+    return null;
+  }
+  return value;
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
-)
+  </React.StrictMode>,
+);
